364bb2af3a0c9c12591863a08078da6337883d69
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-06-09 18:51:16 +0200

    trainer-details: GET, PUT, DELETE methods

--- pokeapp/views.py
+++ pokeapp/views.py
@@ -14,6 +14,7 @@ def trainer_list(request):
 		trainer = Trainer.objects.all()
 		serializer = TrainerSerializer(trainer, many=True)
 		return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
 	elif request.method == 'POST':
 		try:
 			data = JSONParser().parse(request)
@@ -28,3 +29,33 @@ def trainer_list(request):
 	else:
 		return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+
+@csrf_exempt
+def trainer_details(request, pk):
+	try:
+		trainer = Trainer.objects.get(pk=pk)
+	except Trainer.DoesNotExist:
+		return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+	if request.method == 'GET':
+		serializer = TrainerSerializer(trainer)
+		return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+	elif request.method == 'PUT':
+		try:
+			data = JSONParser().parse(request)
+		except ParseError:
+			return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
+		serializer = TrainerSerializer(trainer, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+		else:
+			return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+	elif request.method == 'DELETE':
+		trainer.delete()
+		return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+	else:
+		return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
