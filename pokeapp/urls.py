f14ec22384cdc2181dd84e77f659bc3a6a1211cd
Antoine Pelletier <pelletier.antoinepro@gmail.com>
2017-06-09 18:32:43 +0200

    Working on authentification

--- pokeapp/urls.py
+++ pokeapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'pokeapp'
 
 urlpatterns = [
+	url(r'^login$', views.login, name='login'),
 	url(r'^trainers/$', views.trainer_list, name='trainer-list')
 ]
--- pokeapp/views.py
+++ pokeapp/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import authenticate
+from base64 import b64decode
+from .auth import get_or_create_token, get_basic_auth
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 
@@ -12,3 +15,15 @@ def trainer_list(request):
 		trainer = Trainer.objects.all()
 		serializer = TrainerSerializer(trainer, many=True)
 		return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+def login(request):
+	basic = get_basic_auth(request)
+	if basic is not None:
+		log = b64decode(bytes(basic, 'ascii')).decode('ascii').split(';')
+		user = authenticate(username=log[0], password=log[1])
+		if user is not None:
+			token = get_or_create_token(user)
+			return JsonResponse(data={'token': token.hash})
+	return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
