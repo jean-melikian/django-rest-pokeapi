425b4ab07300c839a75fdf9d84054753f6d71e7c
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-06-09 18:04:57 +0200

    trainer_list: POST method

--- pokeapp/urls.py
+++ pokeapp/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'pokeapp'
 
 urlpatterns = [
-	url(r'^trainers/$', views.trainer_list, name='trainer-list')
+	url(r'^trainers', views.trainer_list, name='trainer-list')
 ]
--- pokeapp/views.py
+++ pokeapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.exceptions import ParseError
+from rest_framework.parsers import JSONParser
 
 from pokeapp.models import Trainer
 from pokeapp.serializers import TrainerSerializer
@@ -12,3 +14,17 @@ def trainer_list(request):
 		trainer = Trainer.objects.all()
 		serializer = TrainerSerializer(trainer, many=True)
 		return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+	elif request.method == 'POST':
+		try:
+			data = JSONParser().parse(request)
+		except ParseError:
+			return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+		serializer = TrainerSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+		else:
+			return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	else:
+		return HttpResponse(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
